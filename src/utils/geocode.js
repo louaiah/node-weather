6d3448d730a415cd61cc5491c3cfca8d83eb8dad
louaiah <louayah318@gmail.com>
2023-02-06 17:35:11 -0500

    init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibG91YWlhaCIsImEiOiJjbGM5bzk1amUxdnNsM3dxa2J2ODVoN3g4In0.6mbiPrTkZYN7myZly2gLSA&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
